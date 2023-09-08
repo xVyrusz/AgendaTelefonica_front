ab2ed7c02a0fe33d569eed34c683d280c87216eb
xVyrusz <cisnnerosbcrra@hotmail.com>
2023-09-07 21:43:23 -0600

    Resgister Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>Home Page</h1>} />
-        <Route path='/login' element={<h1>Login</h1>} />
-        <Route path='/register' element={<h1>Register</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
         <Route path='/phoneNumbers' element={<h1>Phone Numbers</h1>} />
         <Route path='/phoneNumbers/:id' element={<h1>Update Phone Number</h1>} />
         <Route path='/add-phoneNumbers' element={<h1>Add Phone Number</h1>} />
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,41 @@
+import { useForm } from 'react-hook-form'
+
+function RegisterPage() {
+
+    const { register, handleSubmit } = useForm()
+
+    return (
+        <div className="container-fluid dark-bg text-light py-5 min-vh-100 d-flex align-items-center">
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="form-container bg-success p-4 rounded">
+                            <h2 className="text-center">Register</h2>
+                            <form onSubmit={handleSubmit((values) => {
+                                console.log(values);
+                            })}>
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label">Name</label>
+                                    <input type="text" className="form-control" {...register('name', { required: true })} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">Email address</label>
+                                    <input type="email" className="form-control" {...register('email', { required: true })} placeholder="name@example.com" />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">password</label>
+                                    <input type="password" className="form-control"{...register('password', { required: true })} placeholder="Password" autoComplete="off" />
+                                </div>
+                                <div className="text-center">
+                                    <button type="submit" className="btn btn-primary">Register</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RegisterPage
